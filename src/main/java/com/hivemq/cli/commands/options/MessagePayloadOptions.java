0aaaceda577ca53594b2cde658fe42024f381e78
Simon Thum <sthum@meddv.de>
2022-01-13 18:07:45 +0100

    Publish: Allow messages to be read from a file using -m:file
    The file is read as a message body using -m:file (like -pw:file)
    or --message-file. The feature is availble in commandoline and the
    interactive shell.
    
    te fixup: review comments

---
+++ src/main/java/com/hivemq/cli/commands/options/MessagePayloadOptions.java
@@ -0,0 +1,28 @@
+package com.hivemq.cli.commands.options;
+
+import com.hivemq.cli.converters.ByteBufferConverter;
+import com.hivemq.cli.converters.FileToByteBufferConverter;
+import org.jetbrains.annotations.NotNull;
+import picocli.CommandLine;
+
+import java.nio.ByteBuffer;
+
+public class MessagePayloadOptions {
+    @CommandLine.Option(names = {"-m", "--message"}, converter = ByteBufferConverter.class, description = "The message to publish", order = 1)
+    public void setMessageFromCommandline(final @NotNull ByteBuffer messageFromFile) {
+        messageBuffer = messageFromFile;
+    }
+
+    @CommandLine.Option(names = {"-m:file", "--message-file"}, converter = FileToByteBufferConverter.class, description = "The message read in from a file", order = 1)
+    public void setMessageFromFile(final @NotNull ByteBuffer messageFromFile) {
+        messageBuffer = messageFromFile;
+    }
+
+    @NotNull
+    private ByteBuffer messageBuffer;
+
+    @NotNull
+    public ByteBuffer getMessageBuffer() {
+        return messageBuffer;
+    }
+}
